0e99d79b1933783b5e267b2081de8459a720b2b0
imLivai <128120373+imLivai@users.noreply.github.com>
2024-01-28 02:13:00 +0100

    Hotfix

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -10,7 +10,7 @@ public class PlayerManager : MonoBehaviour, IDamageable
     [SerializeField] private float laughTimeReuction;
     [SerializeField] private float laughTimer;
 
-    [SerializeField] private float _currentHealth;
+    [SerializeField] public float _currentHealth;
 
     private PlayerInput _playerInput; // Reference to the PlayerInput component
     private bool isLaughing = false;
